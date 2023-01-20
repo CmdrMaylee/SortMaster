d70af22e7620e99ca22b5b64cb431b96bcb7074d
Jimbob <jimmie.kleman@acgdevs.se>
2023-01-20 14:38:15 +0100

    Array handler class

---
+++ backend/src/Actions/ArrayHandler.cs
@@ -0,0 +1,40 @@
+class ArrayHandler
+{
+    public int[] CreateOrderedArray(int size)
+    {
+        int[] result = new int[size];
+        for (int i = 0; i < result.Length; i++)
+            result[i] = i;
+
+        return result;
+    }
+
+    public int[] ScrambleArray(int[] arr)
+    {
+        Random randy = new();
+
+        for (int scrambles = 0; scrambles < 5; scrambles++)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int randomIndex = randy.Next(arr.Length);
+
+                int temp = arr[i];
+                arr[i] = arr[randomIndex];
+                arr[randomIndex] = temp;
+            }
+
+        }
+
+        return arr;
+    }
+
+    public bool CheckSorted(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+            if (arr[i] < arr[i + 1]) return false;
+
+        return true;
+    }
+}
