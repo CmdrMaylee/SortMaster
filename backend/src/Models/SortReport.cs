dba8011c72afc864d2b3f730c1c18e97a5d037c4
Jimbob <jimmie.kleman@acgdevs.se>
2023-01-20 14:40:09 +0100

    Type for reporting sorting runs

---
+++ backend/src/Models/SortReport.cs
@@ -0,0 +1,10 @@
+namespace src.Models;
+
+public class SortReport
+{
+    public int AlgorithmId;
+    public long TimesCompared;
+    public long ArrayAccesses;
+
+    public long? SortingAttempts;
+}
