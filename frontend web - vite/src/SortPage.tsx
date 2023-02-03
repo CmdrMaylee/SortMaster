f9dfaf39b300583ced5295bfe67e4694a20bd4f9
Jimbob <jimmie.kleman@acgdevs.se>
2023-02-03 11:03:10 +0100

    Fetch algorithms from API(requires cors disabled)

--- frontend web - vite/src/SortPage.tsx
+++ frontend web - vite/src/SortPage.tsx
@@ -4,6 +4,15 @@ import Bars from "./components/Bars";
 export default function SortPage() {
     const [arrSize, setArrSize] = useState(2);
     const [arr, setArr] = useState([1, 2]);
+    const [algorithms, setAlgorithms] = useState([]);
+
+    const getAlgorithms = () => {
+        fetch("http://localhost:5160/api/Sort/GetAllSortingAlgorithms")
+            .then((response) => response.json())
+            .then((json) => {
+                setAlgorithms(json);
+            });
+    };
 
     const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setArrSize(Number(e.target.value));
@@ -20,19 +29,29 @@ export default function SortPage() {
         updateArray();
     }, [arrSize]);
 
+    useEffect(() => {
+        getAlgorithms();
+    }, []);
+
     return (
         <>
-            <Bars arr={arr} visualHeight={50} />
-            <input
-                id="slals"
-                type="range"
-                min={2}
-                max={10}
-                onChange={handleSliderChange}
-                value={arrSize}
-                title={"arrSizeInput"}
-            />
-            <p>{arrSize}</p>
+            <select name="Algorithms">
+                {algorithms.map((x) => (
+                    <option>{x}</option>
+                ))}
+            </select>
+            <Bars arr={arr} visualHeight={5} />
+            <div>
+                <input
+                    type="range"
+                    min={2}
+                    max={10}
+                    onChange={handleSliderChange}
+                    value={arrSize}
+                    title={"arrSizeInput"}
+                />
+                <p>{arrSize}</p>
+            </div>
         </>
     );
 }
