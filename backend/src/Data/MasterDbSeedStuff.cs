2ac6db255fdb4290d38d756c2621836052092295
Jimbob <jimmie.kleman@acgdevs.se>
2023-02-03 10:11:46 +0100

    Hid the seed-data setup for the time being

--- backend/src/Data/MasterDbSeedStuff.cs
+++ backend/src/Data/MasterDbSeedStuff.cs
@@ -5,21 +5,22 @@ namespace src.Data;
 
 public class MasterDbSeedStuff
 {
-    public static async Task SeedAsync(IServiceProvider serviceProvider)
+    /* public static async Task SeedAsync(IServiceProvider serviceProvider)
     {
         using (var context = new MasterDbContext(
             serviceProvider.GetRequiredService<
                 DbContextOptions<MasterDbContext>>()
         ))
 
-            await context.SortHistories.AddAsync(new SortHistory()
+            await context.SortHistories.AddAsync(new SortHistory(new int[] { 1, 2, 3, 4, 5, 6, 7 })
             {
                 Id = new Guid("00000000-0000-0000-0000-000000000000"),
                 AlgorithmId = 1,
+
                 ArrayAccesses = 1000,
                 TimesCompared = 5000,
                 SortStarted = DateTime.Now.AddSeconds(-10),
                 SortEnded = DateTime.Now
             });
-    }
+    } */
 }
--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -31,7 +31,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
 
-    await MasterDbSeedStuff.SeedAsync(services);
+    // await MasterDbSeedStuff.SeedAsync(services);
 }
 
 
