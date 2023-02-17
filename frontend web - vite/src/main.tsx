515e64a733017bcbc0b1072a15b14453675e1c92
Jimbob <jimmie.kleman@acgdevs.se>
2023-02-17 13:11:54 +0100

    Fixed bg color setting

--- frontend web - vite/src/main.tsx
+++ frontend web - vite/src/main.tsx
@@ -6,8 +6,8 @@ import SortPage from "./pages/SortPage";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <div className="h-screen bg-slate-200 dark:bg-slate-800 text-slate-700 dark:text-slate-100">
-            <div className="mx-6">
+        <div className="bg-slate-200 dark:bg-slate-800 text-slate-700 dark:text-slate-100">
+            <div className="md:mx-6">
                 <Header />
                 <SortPage />
             </div>
