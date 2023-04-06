de713277388e2e2b9afaba75bccd3c3b326fead6
Jimbob <jimmie.kleman@acgdevs.se>
2023-04-06 12:57:24 +0200

    wot?

--- backend/src/Models/Dto/AlgorithmDto.cs
+++ backend/src/Models/Dto/AlgorithmDto.cs
@@ -2,10 +2,10 @@ namespace src.Models.Dto;
 
 public class AlgorithmDto
 {
-    public string AlgorithmName { get; set; }
-    public string BigONotationBest { get; set; }
-    public string BigONotationWorst { get; set; }
-    public string BigONotationAverage { get; set; }
-    public string DescriptionText { get; set; }
-    public string PerformanceText { get; set; }
+    public string? AlgorithmName { get; set; }
+    public string? BigONotationBest { get; set; }
+    public string? BigONotationWorst { get; set; }
+    public string? BigONotationAverage { get; set; }
+    public string? DescriptionText { get; set; }
+    public string? PerformanceText { get; set; }
 }
